e517abef4e4dad2700311d3426315c69d50d3644
dspanteleev <dspanteleev@domclick.ru>
2023-08-03 19:43:29 +0300

    fix for java test

--- app-ktor/src/commonTest/kotlin/ru.dpanteleev.otus_kotlin/ApplicationTest.kt
+++ app-ktor/src/commonTest/kotlin/ru.dpanteleev.otus_kotlin/ApplicationTest.kt
@@ -1,18 +1,21 @@
 package ru.dpanteleev.otus_kotlin
 
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import io.ktor.http.*
-import io.ktor.server.testing.*
+import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.testApplication
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class ApplicationTest {
 
-    @Test
-    fun `root endpoint`() = testApplication {
-        val response = client.get("/")
-        assertEquals(HttpStatusCode.OK, response.status)
-        assertEquals("Hello My World!", response.bodyAsText())
-    }
+	@Test
+	fun `root endpoint`() = testApplication {
+		this.application{
+			this.module()
+		}
+		val response = client.get("/")
+		assertEquals(HttpStatusCode.OK, response.status)
+		assertEquals("Hello My World!", response.bodyAsText())
+	}
 }
