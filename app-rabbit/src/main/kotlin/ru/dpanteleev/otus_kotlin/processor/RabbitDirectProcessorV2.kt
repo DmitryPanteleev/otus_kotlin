2646634cc5c3ff674cfa80d536885f4803f4af35
dspanteleev <dspanteleev@domclick.ru>
2023-09-10 17:04:11 +0300

    fix tests

--- app-rabbit/src/main/kotlin/ru/dpanteleev/otus_kotlin/processor/RabbitDirectProcessorV2.kt
+++ app-rabbit/src/main/kotlin/ru/dpanteleev/otus_kotlin/processor/RabbitDirectProcessorV2.kt
@@ -2,14 +2,24 @@ package ru.dpanteleev.otus_kotlin.processor
 
 import com.rabbitmq.client.Channel
 import com.rabbitmq.client.Delivery
+import ru.dpanteleev.otus_kotlin.AppSettings
+import ru.dpanteleev.otus_kotlin.RabbitProcessorBase
+import ru.dpanteleev.otus_kotlin.apiV2RequestDeserialize
+import ru.dpanteleev.otus_kotlin.apiV2ResponseSerialize
 import ru.dpanteleev.otus_kotlin.config.RabbitConfig
 import ru.dpanteleev.otus_kotlin.config.RabbitExchangeConfiguration
-import ru.dpanteleev.otus_kotlin.config.corSettings
+import ru.dpanteleev.otus_kotlin.config.coreSettings
+import ru.dpanteleev.otus_kotlin.config.rabbitLogger
+import ru.dpanteleev.otus_kotlin.fromTransport
+import ru.dpanteleev.otus_kotlin.models.IRequest
+import ru.dpanteleev.otus_kotlin.process
+import ru.dpanteleev.otus_kotlin.toLog
+import ru.dpanteleev.otus_kotlin.toTransport
 
 class RabbitDirectProcessorV2(
     config: RabbitConfig,
     processorConfig: RabbitExchangeConfiguration,
-    settings: MkplAppSettings = corSettings,
+    settings: AppSettings = coreSettings,
 ) : RabbitProcessorBase(config, processorConfig) {
 
     private val logger = settings.logger.logger(RabbitDirectProcessorV1::class)
@@ -25,7 +35,7 @@ class RabbitDirectProcessorV2(
             },
             {
                 rabbitLogger.info("start publish")
-                val response = toTransportAd()
+                val response = toTransport()
                 apiV2ResponseSerialize(response).also {
                     println("Publishing $response to ${processorConfig.exchange} exchange for keyOut ${processorConfig.keyOut}")
                     basicPublish(processorConfig.exchange, processorConfig.keyOut, null, it.toByteArray())
